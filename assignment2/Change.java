cf341d56b05c6181acc79b42a6c41e53f394ce0e
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-27 14:23:25 -0600

    First Implementation of GetChange

--- assignment2/Change.java
+++ assignment2/Change.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
-        //write your code here
-        return m;
-    }
+  private static int getChange(int m) {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
+    int[] denominations = {10, 5, 1};
+    int c = 0;  // Index of denomination
+    int numberOfCoins = 0;
+
+    while (m > 0) {
+      if (m >= denominations[c]) {
+        m = m - denominations[c];
+        numberOfCoins = numberOfCoins + 1;
+      }
+      else {
+        c = c + 1;
+      }
 
     }
+    return numberOfCoins;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int m = scanner.nextInt();
+    System.out.println(getChange(m));
+
+  }
 }
 
