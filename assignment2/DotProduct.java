a5741fe9895e7bbcd28d43f3cbe37a8409f4d716
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-28 10:39:02 -0600

    Implement MaxDotProduct naive implementation

--- assignment2/DotProduct.java
+++ assignment2/DotProduct.java
@@ -1,27 +1,37 @@
 import java.util.*;
 
 public class DotProduct {
-    private static long maxDotProduct(int[] a, int[] b) {
-        //write your code here
-        long result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
-        }
-        return result;
-    }
+  private static long maxDotProduct(int[] a, int[] b) {
+    // n² implementation
+    long result = 0;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
+    for (int i = 0; i < a.length; i++) {
+      long currentMaxProd = a[i] + b[0];
+
+      for (int j = 0; j < b.length; j++) {
+        long prod = i * j;
+        if (prod > currentMaxProd) {
+          currentMaxProd = prod;
         }
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++) {
-            b[i] = scanner.nextInt();
-        }
-        System.out.println(maxDotProduct(a, b));
+      }
+      
+      result += currentMaxProd;
     }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = scanner.nextInt();
+    }
+    int[] b = new int[n];
+    for (int i = 0; i < n; i++) {
+      b[i] = scanner.nextInt();
+    }
+    System.out.println(maxDotProduct(a, b));
+  }
 }
 
