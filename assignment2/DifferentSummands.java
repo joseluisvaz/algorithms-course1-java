ddca95ed070c5c53c386760b93472fad26f7aee4
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-28 15:43:57 -0600

    First functional implementation of optimalsummands

--- assignment2/DifferentSummands.java
+++ assignment2/DifferentSummands.java
@@ -1,20 +1,31 @@
 import java.util.*;
 
 public class DifferentSummands {
-    private static List<Integer> optimalSummands(int n) {
-        List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
-        return summands;
+  private static List<Integer> optimalSummands(int n) {
+
+    List<Integer> summands = new ArrayList<>();
+
+    return optimalSummandsInner(n-1, 1, summands);
+  }
+
+  private static List<Integer> optimalSummandsInner(int n, int l, List<Integer> acc) {
+
+    if ( n < (2*l) ) {
+      acc.add(n);
+      return acc;
     }
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        List<Integer> summands = optimalSummands(n);
-        System.out.println(summands.size());
-        for (Integer summand : summands) {
-            System.out.print(summand + " ");
-        }
+    acc.add(l);
+    return optimalSummandsInner(n-1, l+1, acc);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    List<Integer> summands = optimalSummands(n);
+    System.out.println(summands.size());
+    for (Integer summand : summands) {
+      System.out.print(summand + " ");
     }
+  }
 }
 
