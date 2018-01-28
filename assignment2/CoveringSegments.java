35bb6366f315a0c10fc1fccf0c36ade2d1d223d0
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-28 14:46:41 -0600

    Implement SegmentComparator as classmethod

--- assignment2/CoveringSegments.java
+++ assignment2/CoveringSegments.java
@@ -4,14 +4,14 @@ public class CoveringSegments {
 
   private static Integer[] optimalPoints(Segment[] segments) {
 
-    Arrays.sort(segments, new SegmentComparator());
+    Arrays.sort(segments, new Segment.SegmentComparator());
     List<Integer> points = new ArrayList<>();
 
     // Initialize first point in array
     Integer point = segments[0].end;
     points.add(point);
 
-    for (Segment seg: segments) {
+    for (Segment seg : segments) {
       if (!(point <= seg.end && point >= seg.start)) {
         point = seg.end;
         points.add(point);
@@ -36,29 +36,36 @@ public class CoveringSegments {
     public Integer getEnd() {
       return this.end;
     }
-  }
 
-  public static class SegmentComparator implements Comparator<Segment> {
-    public int compare(Segment seg1, Segment seg2) {
-      return seg1.getEnd().compareTo(seg2.getEnd());
+    public static class SegmentComparator implements Comparator<Segment> {
+
+      public int compare(Segment seg1, Segment seg2) {
+        return seg1.getEnd().compareTo(seg2.getEnd());
+      }
     }
   }
 
+
   public static void main(String[] args) {
+
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     Segment[] segments = new Segment[n];
+
     for (int i = 0; i < n; i++) {
       int start, end;
       start = scanner.nextInt();
       end = scanner.nextInt();
       segments[i] = new Segment(start, end);
     }
+
     Integer[] points = optimalPoints(segments);
     System.out.println(points.length);
+
     for (int point : points) {
       System.out.print(point + " ");
     }
+
   }
 }
  
