26acd751352c4f0ca3cb905a8654745eedb3eafa
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-01-28 12:58:39 -0600

    Implement sort for class Segments

--- assignment2/CoveringSegments.java
+++ assignment2/CoveringSegments.java
@@ -2,39 +2,60 @@ import java.util.*;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
+  private static int[] optimalPoints(Segment[] segments) {
+    //write your code here
+
+    Arrays.sort(segments, new SegmentComparator());
+
+    int[] points = new int[2 * segments.length];
+    /*
+    for (int i = 0; i < segments.length; i++) {
+      points[2 * i] = segments[i].start;
+      points[2 * i + 1] = segments[i].end;
+    }
+    */
+    System.out.println(segments[0].getEnd());
+    return points;
+  }
+
+  private static class Segment {
+    int start, end;
+
+    Segment(int start, int end) {
+      this.start = start;
+      this.end = end;
     }
 
-    private static class Segment {
-        int start, end;
+    public Integer getStart() {
+      return this.start;
+    }
 
-        Segment(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
+    public Integer getEnd() {
+      return this.end;
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        Segment[] segments = new Segment[n];
-        for (int i = 0; i < n; i++) {
-            int start, end;
-            start = scanner.nextInt();
-            end = scanner.nextInt();
-            segments[i] = new Segment(start, end);
-        }
-        int[] points = optimalPoints(segments);
-        System.out.println(points.length);
-        for (int point : points) {
-            System.out.print(point + " ");
-        }
+  }
+
+  public static class SegmentComparator implements Comparator<Segment> {
+    public int compare(Segment seg1, Segment seg2) {
+      return seg1.getEnd().compareTo(seg2.getEnd());
     }
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    Segment[] segments = new Segment[n];
+    for (int i = 0; i < n; i++) {
+      int start, end;
+      start = scanner.nextInt();
+      end = scanner.nextInt();
+      segments[i] = new Segment(start, end);
+    }
+    int[] points = optimalPoints(segments);
+    System.out.println(points.length);
+    for (int point : points) {
+      System.out.print(point + " ");
+    }
+  }
 }
  
